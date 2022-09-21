2fa49d056acdbf914854febf019e2e0dd0067b78
Aakash Panchal <aakashpanchal7000@gmail.com>
2022-09-21 21:58:12 +0530

    update locomotive scroll on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useLocation, Route, Routes } from "react-router-dom";
-import LocoMotiveScroll from "./components/Hooks/LocoMotiveScroll.ts";
+import LocomotiveScroll from "locomotive-scroll";
+import "locomotive-scroll/dist/locomotive-scroll.css";
 import NavBar from "./components/subComponents/NavBar";
 import Home from "./components/Pages/Home";
 import About from "./components/Pages/About";
@@ -9,27 +10,20 @@ import Contact from "./components/Pages/Contact";
 import styled from "styled-components";
 import Cursor from "./components/CustomCursor/Cursor";
 import ViewProject from "./components/CustomCursor/CursorContext";
-import "./App.scss";
 import PageTransition from "./components/PageTransition";
 import ErrorPage from "./components/Pages/ErrorPage";
 import Quotes from "./components/subComponents/Quotes";
-// import LocomotiveScroll from "locomotive-scroll";
-// import "locomotive-scroll/dist/locomotive-scroll.css";
-
-// const scroll = new LocomotiveScroll({
-//   el: document.querySelector("[data-scroll-container]"),
-//   smooth: true,
-// });
-// console.log("hello");
+import "./App.scss";
 
 function App() {
   const [viewProject, setViewProject] = useState(false);
   const scrollRef = useRef(null);
   const location = useLocation();
+  const [scrollbar, setScrollbar] = useState(null);
 
-  LocoMotiveScroll(
-    {
-      ref: scrollRef,
+  useEffect(() => {
+    const LocoScroll = new LocomotiveScroll({
+      el: scrollRef.current,
       smooth: true,
       scrollFromAnywhere: true,
       reloadOnContextChange: true,
@@ -37,9 +31,25 @@ function App() {
         smooth: true,
       },
       tablet: { smooth: true },
-    },
-    [location]
-  );
+      repeat: true,
+    });
+    setScrollbar(LocoScroll);
+  }, []);
+
+  useEffect(() => {
+    if (scrollbar !== null) {
+      scrollbar.update();
+      scrollbar.scrollTo("top", {
+        offset: 0,
+        callback: function () {
+          // do something...
+        },
+        duration: 600,
+        easing: [0.25, 0.0, 0.35, 1.0],
+        disableLerp: true,
+      });
+    }
+  }, [location]);
 
   return (
     <>
@@ -71,61 +81,6 @@ const Container = styled.div`
   width: 100vw;
 `;
 
-const BgNoise = styled.div`
-  /* background: #101010;
-  pointer-events: none;
-  height: 100vh;
-  width: 100vw;
-  overflow: hidden;
-  &:before {
-    position: absolute;
-    content: "";
-    top: -10rem;
-    left: -10rem;
-    width: calc(100% + 20rem);
-    height: calc(100% + 20rem);
-    z-index: -1;
-    pointer-events: none;
-    touch-action: none;
-    background-image: url("https://res.cloudinary.com/dbov4oeyp/image/upload/v1663052723/Portfolio/bg-noise_x8xg7l.png");
-    background-position: 50%;
-    animation: BgNoise 1s steps(2) infinite;
-  } */
-  @keyframes BgNoise {
-    0% {
-      transform: translate3d(0, 9rem, 0);
-    }
-    10% {
-      transform: translate3d(-1rem, -4rem, 0);
-    }
-    20% {
-      transform: translate3d(-8rem, 2rem, 0);
-    }
-    30% {
-      transform: translate3d(9rem, -9rem, 0);
-    }
-    40% {
-      transform: translate3d(-2rem, 7rem, 0);
-    }
-    50% {
-      transform: translate3d(-9rem, -4rem, 0);
-    }
-    60% {
-      transform: translate3d(2rem, 6rem, 0);
-    }
-    70% {
-      transform: translate3d(7rem, -8rem, 0);
-    }
-    80% {
-      transform: translate3d(-9rem, 1rem, 0);
-    }
-    90% {
-      transform: translate3d(6rem, -5rem, 0);
-    }
-    100% {
-      transform: translate3d(-7rem, 0, 0);
-    }
-  }
-`;
+const BgNoise = styled.div``;
 
 export default App;
