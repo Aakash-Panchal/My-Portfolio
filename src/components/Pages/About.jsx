6fe6505a3fbdefd206adfdc696c3ce39b2dbd98f
Aakash Panchal <aakashpanchal7000@gmail.com>
2022-09-22 12:59:55 +0530

    delete title component

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Title from "../subComponents/Title";
 
 const About = () => {
   return (
     <div>
-      <Title Title="ABOUT ME" />
-
       <Link to="/">Home</Link>
     </div>
   );
