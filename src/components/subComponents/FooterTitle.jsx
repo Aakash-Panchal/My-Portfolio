3a7e6a1acdd213a64f30faa7671d7b909803c7a8
Aakash Panchal <aakashpanchal7000@gmail.com>
2022-10-14 18:59:11 +0530

    improve footerTitle component

--- src/components/subComponents/FooterTitle.jsx
+++ src/components/subComponents/FooterTitle.jsx
@@ -6,88 +6,101 @@ const FooterTitle = ({ title, titleLink }) => {
   return (
     <Title data-scroll-section>
       <Link
+        to={titleLink}
         data-scroll
         data-scroll-direction="horizontal"
-        data-scroll-speed="-2"
-        to={titleLink}
+        data-scroll-speed="12"
       >
+        {title} <span>{title}</span>
         {title}
+        <span>{title}</span>
+        {title} <span>{title}</span>
+        {title} <span>{title}</span>
+        {title}
+        <span>{title}</span>
+        {title} <span>{title}</span>
+        {title}
+        <span>{title}</span>
+        {title} <span>{title}</span>
+        {title} <span>{title}</span>
+        {title}
+        <span>{title}</span>
       </Link>
       <Link
+        to={titleLink}
         data-scroll
         data-scroll-direction="horizontal"
-        data-scroll-speed="-2"
-        to={titleLink}
+        data-scroll-speed="-6"
       >
+        <span>{title} </span>
         {title}
+        <span>{title} </span>
+        {title} <span>{title} </span>
+        {title} <span>{title}</span>
+        {title}
+        <span>{title}</span>
+        {title} <span>{title}</span>
+        {title}
+        <span>{title}</span>
+        {title} <span>{title}</span>
+        {title} <span>{title}</span>
+        {title}
+        <span>{title}</span>
       </Link>
       <Link
+        to={titleLink}
         data-scroll
         data-scroll-direction="horizontal"
-        data-scroll-speed="-2"
-        to={titleLink}
+        data-scroll-speed="12"
       >
+        {title} <span>{title}</span>
         {title}
-      </Link>
-      <Link
-        data-scroll
-        data-scroll-direction="horizontal"
-        data-scroll-speed="-2"
-        to={titleLink}
-      >
+        <span>{title}</span>
+        {title} <span>{title}</span>
+        {title} <span>{title}</span>
         {title}
-      </Link>
-      <Link
-        data-scroll
-        data-scroll-direction="horizontal"
-        data-scroll-speed="-2"
-        to={titleLink}
-      >
+        <span>{title}</span>
+        {title} <span>{title}</span>
         {title}
-      </Link>
-      <Link
-        data-scroll
-        data-scroll-direction="horizontal"
-        data-scroll-speed="-2"
-        to={titleLink}
-      >
+        <span>{title}</span>
+        {title} <span>{title}</span>
+        {title} <span>{title}</span>
         {title}
-      </Link>
-      <Link
-        data-scroll
-        data-scroll-direction="horizontal"
-        data-scroll-speed="-2"
-        to={titleLink}
-      >
-        {title}
+        <span>{title}</span>
       </Link>
     </Title>
   );
 };
 
 const Title = styled.div`
+  font-family: "Open Sans", sans-serif;
+  margin: 10rem 0;
   min-width: 100%;
-  height: 25rem;
-  display: flex;
-  flex-wrap: nowrap;
-  white-space: nowrap;
-  justify-content: center;
-  align-items: flex-start;
-  flex-shrink: 0;
   overflow: hidden;
   a {
+    display: flex;
+    flex-wrap: nowrap;
+    white-space: nowrap;
+    justify-content: center;
+    align-items: flex-start;
+    text-transform: uppercase;
+    flex-shrink: 0;
+    font-size: 6rem;
+    font-weight: 700;
+    letter-spacing: 2px;
+    display: flex;
+    width: 200vw;
+    flex-wrap: nowrap;
+    white-space: nowrap;
     color: #fff;
-    font-size: 8rem;
-    padding: 0 10rem;
-    &:first-letter {
-      font-style: italic;
+    span {
+      opacity: 0.3;
+      padding: 0 1rem;
     }
   }
   @media screen and (max-width: 768px) {
-    height: 10rem;
     a {
       font-size: 3rem;
-      padding: 0 2rem;
     }
   }
 `;
