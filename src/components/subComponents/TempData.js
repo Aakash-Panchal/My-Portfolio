315d2b94e352f24f8b8a8ed8ad4551971970f5b1
Aakash Panchal <aakashpanchal7000@gmail.com>
2022-10-14 19:13:53 +0530

    add temp dummy data

---
+++ src/components/subComponents/TempData.js
@@ -0,0 +1,18 @@
+const TempData = [
+  {
+    title: "I want to work with you, how do we get started?",
+    content: `To start a new project just fill in the contact form and let me know what you want to get done.`,
+  },
+  {
+    title: "How long will it take to complete my project?",
+    content: `That depends on the type of project.`,
+  },
+  {
+    title: "Section 3",
+    content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
+      quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
+      dolor ut sequi minus iste? Quas?`,
+  },
+];
+
+export default TempData;
