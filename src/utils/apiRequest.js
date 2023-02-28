a6f1a549c7266e2a2c1d403a9fb9fa8ae7e2f231
Aakash Panchal <aakashpanchal7000@gmail.com>
2023-02-28 10:06:16 +0530

    update api url

--- src/utils/apiRequest.js
+++ src/utils/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "https://api.aakashpanchal.com/api/",
   withCredentials: true,
 });
 
